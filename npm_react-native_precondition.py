d0a5c5333a8d6116478fc3e7c3fa50749f24ed66
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-22 18:22:37 +0900

    Added react-native-vector-icons setting process.

--- npm_react-native_precondition.py
+++ npm_react-native_precondition.py
@@ -21,7 +21,6 @@ command = [
     "npm i @gluestack-ui/overlay",
     "npm i @gluestack-ui/themed",
     "npm i @gluestack-ui/toast",
-    "npx pod-install ios",
 ]
 
 # ChagkaUI Doc: https://chakra-ui.com/getting-started
@@ -84,6 +83,9 @@ for cmd in command:
     print(f"{'*'*10} {cmd} is done. {'*'*10}")
     time.sleep(0.5)
 
+# App.tsx, prettier, eslint setting
+
+print(f"{'*'*10} App.tsx, prettier, eslint start to set. {'*'*10}")
 created_path = os.path.join(PATH, NAME)
 os.chdir(created_path)
 time.sleep(1)
@@ -155,6 +157,99 @@ module.exports = {
 with open(eslint, "w") as eslint_file:
     eslint_file.write(replacement_eslint_code)
 
+print(f"{'*'*10} App.tsx, prettier, eslint set is done. {'*'*10}")
+
+# ios react-native-vector-icons setting
 print(
-    "react-native-vector-icons requires additional settings.\nPlease check docs and set it."
+    f"{'*'*10} ios Info.plist start to set for using react-native-vector-icons. {'*'*10}"
 )
+
+ios_project_path = os.path.join("ios", NAME)
+info_plist_path = os.path.join(created_path, ios_project_path)
+
+os.chdir(info_plist_path)
+time.sleep(1)
+
+# Info.plist 파일 읽기
+info_plist = "Info.plist"
+
+with open(info_plist, "r", encoding="utf-8") as file:
+    lines = file.readlines()
+
+# 찾을 문자열
+search_string = "</dict>"
+
+# 찾은 문자열의 인덱스
+index = None
+
+# 뒤에서부터 역순으로 검색하여 가장 끝에 있는 </dict> 찾기
+for i in range(len(lines) - 1, -1, -1):
+    if search_string in lines[i]:
+        index = i
+        break
+
+# 추가할 Key-Values 선언
+new_key_values = """
+    <key>UIAppFonts</key>
+    <array>
+		<string>AntDesign.ttf</string>
+		<string>Entypo.ttf</string>
+		<string>EvilIcons.ttf</string>
+		<string>Feather.ttf</string>
+		<string>FontAwesome.ttf</string>
+		<string>FontAwesome5_Brands.ttf</string>
+		<string>FontAwesome5_Regular.ttf</string>
+		<string>FontAwesome5_Solid.ttf</string>
+		<string>FontAwesome6_Brands.ttf</string>
+		<string>FontAwesome6_Regular.ttf</string>
+		<string>FontAwesome6_Solid.ttf</string>
+		<string>Foundation.ttf</string>
+		<string>Ionicons.ttf</string>
+		<string>MaterialIcons.ttf</string>
+		<string>MaterialCommunityIcons.ttf</string>
+		<string>SimpleLineIcons.ttf</string>
+		<string>Octicons.ttf</string>
+		<string>Zocial.ttf</string>
+		<string>Fontisto.ttf</string>
+	</array>
+"""
+
+# 해당 라인 뒤에 새로운 Key-Values 추가
+lines.insert(index, new_key_values)
+
+# 수정된 내용을 파일에 쓰기
+with open(info_plist, "w", encoding="utf-8") as file:
+    file.writelines(lines)
+
+print(f"{'*'*10} ios Info.plist set is done. {'*'*10}")
+
+# android react-native-vector-icons setting
+print(
+    f"{'*'*10} android build.gradle start to set for using react-native-vector-icons. {'*'*10}"
+)
+
+android_app_path = os.path.join(created_path, "android/app")
+
+os.chdir(android_app_path)
+time.sleep(1)
+
+build_gradle = "build.gradle"
+
+add_android_method = (
+    "apply from: file('../../node_modules/react-native-vector-icons/fonts.gradle')"
+)
+
+with open(build_gradle, "a") as build_gradle_file:
+    build_gradle_file.write(add_android_method)
+
+print(f"{'*'*10} android build.gradle set is done. {'*'*10}")
+
+print(f"{'*'*10} ios pod install start to run commands. {'*'*10}")
+
+os.chdir(created_path)
+time.sleep(1)
+
+os.system("npx pod-install ios")
+time.sleep(0.5)
+
+print(f"{'*'*10} ios pod install is done. {'*'*10}")
